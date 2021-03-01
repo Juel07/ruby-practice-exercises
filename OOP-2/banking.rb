0c12af542fcca85ee9e7d1e5aef794960ce07f17
Juel B <juel.braganza97@gmail.com>
2021-03-01 00:24:39 +0000

    added comments

--- OOP-2/banking.rb
+++ OOP-2/banking.rb
@@ -1,24 +1,30 @@
 # create a class
 class Account
+
+    # make the :name and :balance attributes readable but not writeable
       attr_reader :name, :balance
       def initialize(name, balance=100)
+
+        # store the parameters in instance variables
         @name = name
         @balance = balance
       end
       
+    # create 2 public methods, one to display the account balance & other to withdraw money from account
       def display_balance(pin_number)
         puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
       end
       
       def withdraw(pin_number, amount)
         if pin_number == pin
-          @balance -= amount
+          @balance -= amount # subtract amount from the balance
           puts "Withdrew #{amount}. New balance: $#{@balance}."
         else
           puts pin_error
         end
       end
       
+    # create 2 private methods; public methods will rely on these
       private
       
       def pin
@@ -30,7 +36,7 @@ class Account
       end
 end
 
-my_account = Account.new("Eric", 1_000_000)
+my_account = Account.new("Eric", 1_000_000)
 my_account.withdraw(11, 500_000)
 my_account.display_balance(1200)
 my_account.withdraw(1200, 500_000)
